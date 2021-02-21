8c29671c8ba4e5b3e038d1dbf6fc1a2d334cc514
David Hambraeus <Ddhambraeus@gmail.com>
2021-02-21 10:04:46 +0100

    Added batch bash file

---
+++ s4batch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Bunch of tetralith-commands
+
+# Set number of cores to be used and which wavelengths are to be simulated
+numberOfCores=3;
+minWavelength=1450
+maxWavelength=1700
+
+# Calculate how many wavelengths each core should simulate
+noPerCore=$(( (maxWavelength - minWavelength + 1)/numberOfCores ))
+rest=$(( (maxWavelength - minWavelength + 1)%numberOfCores ))
+
+# Each core should simulate wavelengths from $first to $last inclusive
+first=$minWavelength
+
+for core in {1..$numberOfCores} 
+do
+    echo $core
+    if [ $core -le $rest ]
+    then
+        last=$((first + noPerCore))
+    else
+        last=$((first + noPerCore - 1))
+    fi
+    tmpFilename="tmp_$core.txt"
+    S4 <(sed '1i wavelength_start=$first\nwavelength_end=$last' s4script.lua) > $tmpFilename &
+    first=$((last + 1))
+done
+
+wait
+
+cat tmp_*.txt > data.txt
